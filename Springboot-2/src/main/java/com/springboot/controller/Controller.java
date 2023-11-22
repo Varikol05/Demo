b902b441c00136be9e4478aba875b5445e49132a
vpravali <vpravali@LIN-B8ZQTN3.corp.capgemini.com>
2023-11-22 20:32:24 +0530

    Commit changes

---
+++ Springboot-2/src/main/java/com/springboot/controller/Controller.java
@@ -0,0 +1,24 @@
+package com.springboot.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.resource.UserValid;
+
+@RestController
+public class Controller {
+	UserValid u = new UserValid();
+	@RequestMapping("/user/{user}/pass/{pass}")
+	public String valid (@PathVariable String user,@PathVariable String pass)
+	{
+		if(u.userval(user, pass))
+			return "valid";
+		else
+			return "invalid";
+	}
+	
+	
+	
+
+}
---
+++ Springboot-2/src/main/java/com/springboot/resource/UserValid.java
@@ -0,0 +1,15 @@
+package com.springboot.resource;
+
+public class UserValid {
+	public boolean userval(String user, String pass)
+	{
+		if(user.equals("pravalika") && pass.equals("abc"))
+				 return true;
+		else
+		return false;
+		
+				 
+	}
+	
+
+}
